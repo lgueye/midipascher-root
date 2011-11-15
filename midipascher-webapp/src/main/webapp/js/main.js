9ae8f174560f8221b50336b4ca53b53d8295e606
louis gueye <louis.gueye@gmail.com>
2011-11-15 18:07:11 +0100

    trying jquery and google maps v3

---
+++ midipascher-webapp/src/main/webapp/js/main.js
@@ -0,0 +1,30 @@
+var geocoder;
+var map;
+var marker;
+
+function initialize() {
+	//	MAP
+	var latlng = new google.maps.LatLng(41.659,-4.714);
+	var options = {
+			zoom: 16,
+			center: latlng,
+			mapTypeId: google.maps.MapTypeId.PLAN
+	};
+
+	map = new google.maps.Map(document.getElementById("map_canvas"), options);
+
+	//GEOCODER
+	geocoder = new google.maps.Geocoder();
+
+	marker = new google.maps.Marker({
+		map: map,
+		draggable: true
+	});
+
+}
+
+$(document).ready(function() {
+
+	initialize();
+
+}
