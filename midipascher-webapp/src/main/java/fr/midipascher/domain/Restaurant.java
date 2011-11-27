62640dc9c56e96399d045cad0f9b0300797722ed
louis gueye <louis.gueye@gmail.com>
2011-11-27 23:51:47 +0100

    fixing tests, added site images

--- midipascher-webapp/src/main/java/fr/midipascher/domain/Restaurant.java
+++ midipascher-webapp/src/main/java/fr/midipascher/domain/Restaurant.java
@@ -35,206 +35,201 @@ import fr.midipascher.domain.validation.Update;
 @XmlRootElement
 public class Restaurant extends AbstractEntity {
 
-    public static final String TABLE_NAME = "restaurant";
-    public static final String TABLE_NAME_RESTAURANT_FOOD_SPECIALTY = "restaurant_food_specialty";
+	public static final String	TABLE_NAME								= "restaurant";
+	public static final String	TABLE_NAME_RESTAURANT_FOOD_SPECIALTY	= "restaurant_food_specialty";
 
-    public static final String COLUMN_NAME_ID = "restaurant_id";
-    public static final String COLUMN_NAME_PHONE_NUMBER = "phone_number";
-    public static final String COLUMN_NAME_MAIN_OFFER = "main_offer";
-    public static final String COLUMN_NAME_COMPANY_ID = "company_id";
+	public static final String	COLUMN_NAME_ID							= "restaurant_id";
+	public static final String	COLUMN_NAME_PHONE_NUMBER				= "phone_number";
+	public static final String	COLUMN_NAME_MAIN_OFFER					= "main_offer";
+	public static final String	COLUMN_NAME_COMPANY_ID					= "company_id";
 
-    public static final int CONSTRAINT_NAME_MAX_SIZE = 50;
-    public static final int CONSTRAINT_DESCRIPTION_MAX_SIZE = 200;
-    public static final int CONSTRAINT_PHONE_NUMBER_MAX_SIZE = 20;
-    public static final int CONSTRAINT_MAIN_OFFER_MAX_SIZE = 200;
-    public static final int CONSTRAINT_COMPANY_ID_MAX_SIZE = 50;
+	public static final int		CONSTRAINT_NAME_MAX_SIZE				= 50;
+	public static final int		CONSTRAINT_DESCRIPTION_MAX_SIZE			= 200;
+	public static final int		CONSTRAINT_PHONE_NUMBER_MAX_SIZE		= 20;
+	public static final int		CONSTRAINT_MAIN_OFFER_MAX_SIZE			= 200;
+	public static final int		CONSTRAINT_COMPANY_ID_MAX_SIZE			= 50;
 
-    /**
+	/**
 	 * 
 	 */
-    private static final long serialVersionUID = -5952533696555432772L;
+	private static final long	serialVersionUID						= -5952533696555432772L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = Restaurant.COLUMN_NAME_ID)
-    @NotNull(message = "{restaurant.id.required}", groups = { Update.class, Delete.class })
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = Restaurant.COLUMN_NAME_ID)
+	@NotNull(message = "{restaurant.id.required}", groups = { Update.class, Delete.class })
+	private Long				id;
 
-    @NotEmpty(message = "{restaurant.name.required}", groups = { Create.class, Update.class })
-    @Size(max = Restaurant.CONSTRAINT_NAME_MAX_SIZE, message = "{restaurant.name.max.size}", groups = { Create.class,
-            Update.class })
-    private String name;
+	@NotEmpty(message = "{restaurant.name.required}", groups = { Create.class, Update.class })
+	@Size(max = Restaurant.CONSTRAINT_NAME_MAX_SIZE, message = "{restaurant.name.max.size}", groups = { Create.class,
+			Update.class })
+	private String				name;
 
-    @Size(max = Restaurant.CONSTRAINT_DESCRIPTION_MAX_SIZE, message = "{restaurant.description.max.size}", groups = {
-            Create.class, Update.class })
-    private String description;
+	@Size(max = Restaurant.CONSTRAINT_DESCRIPTION_MAX_SIZE, message = "{restaurant.description.max.size}", groups = {
+			Create.class, Update.class })
+	private String				description;
 
-    @Column(name = Restaurant.COLUMN_NAME_COMPANY_ID)
-    @NotEmpty(message = "{user.companyId.required}", groups = { Create.class, Update.class })
-    @Size(max = Restaurant.CONSTRAINT_COMPANY_ID_MAX_SIZE, message = "{user.companyId.max.size}", groups = {
-            Create.class, Update.class })
-    private String companyId;
+	@Column(name = Restaurant.COLUMN_NAME_COMPANY_ID)
+	@NotEmpty(message = "{user.companyId.required}", groups = { Create.class, Update.class })
+	@Size(max = Restaurant.CONSTRAINT_COMPANY_ID_MAX_SIZE, message = "{user.companyId.max.size}", groups = {
+			Create.class, Update.class })
+	private String				companyId;
 
-    @Column(name = Restaurant.COLUMN_NAME_PHONE_NUMBER)
-    @NotEmpty(message = "{restaurant.phoneNumber.required}", groups = { Create.class, Update.class })
-    @Size(max = Restaurant.CONSTRAINT_PHONE_NUMBER_MAX_SIZE, message = "{restaurant.phoneNumber.max.size}", groups = {
-            Create.class, Update.class })
-    private String phoneNumber;
+	@Column(name = Restaurant.COLUMN_NAME_PHONE_NUMBER)
+	@NotEmpty(message = "{restaurant.phoneNumber.required}", groups = { Create.class, Update.class })
+	@Size(max = Restaurant.CONSTRAINT_PHONE_NUMBER_MAX_SIZE, message = "{restaurant.phoneNumber.max.size}", groups = {
+			Create.class, Update.class })
+	private String				phoneNumber;
 
-    @Column(name = Restaurant.COLUMN_NAME_MAIN_OFFER)
-    @Size(max = Restaurant.CONSTRAINT_MAIN_OFFER_MAX_SIZE, message = "{restaurant.mainOffer.max.size}", groups = {
-            Create.class, Update.class })
-    private String mainOffer;
+	@Column(name = Restaurant.COLUMN_NAME_MAIN_OFFER)
+	@Size(max = Restaurant.CONSTRAINT_MAIN_OFFER_MAX_SIZE, message = "{restaurant.mainOffer.max.size}", groups = {
+			Create.class, Update.class })
+	private String				mainOffer;
 
-    private boolean kosher;
+	private boolean				kosher;
 
-    private boolean halal;
+	private boolean				halal;
 
-    private boolean vegetarian;
+	private boolean				vegetarian;
 
-    @ManyToMany(cascade = CascadeType.ALL)
-    @JoinTable(name = Restaurant.TABLE_NAME_RESTAURANT_FOOD_SPECIALTY, joinColumns = { @JoinColumn(name = Restaurant.COLUMN_NAME_ID) }, inverseJoinColumns = { @JoinColumn(name = FoodSpecialty.ID_COLUMN_NAME) })
-    @Valid
-    @NotEmpty(message = "{restaurant.specialties.required}", groups = { Create.class, Update.class })
-    private Set<FoodSpecialty> specialties;
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinTable(name = Restaurant.TABLE_NAME_RESTAURANT_FOOD_SPECIALTY, joinColumns = { @JoinColumn(name = Restaurant.COLUMN_NAME_ID) }, inverseJoinColumns = { @JoinColumn(name = FoodSpecialty.ID_COLUMN_NAME) })
+	@Valid
+	@NotEmpty(message = "{restaurant.specialties.required}", groups = { Create.class, Update.class })
+	private Set<FoodSpecialty>	specialties;
 
-    @Valid
-    @NotNull(message = "{restaurant.address.required}", groups = { Create.class, Update.class })
-    private Address address;
+	@Valid
+	@NotNull(message = "{restaurant.address.required}", groups = { Create.class, Update.class })
+	private Address				address;
 
-    @ManyToOne
-    @Valid
-    @NotNull(message = "{restaurant.user.required}", groups = { Create.class, Update.class })
-    private User user;
+	@ManyToOne
+	@Valid
+	@NotNull(message = "{restaurant.user.required}", groups = { Create.class, Update.class })
+	private User				user;
 
-    /**
+	/**
 	 * 
 	 */
-    public Restaurant() {
-        super();
-        setAddress(new Address());
-    }
+	public Restaurant() {
+		super();
+		setAddress(new Address());
+	}
 
-    @Override
-    public boolean equals(final Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        final Restaurant other = (Restaurant) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
-    }
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		final Restaurant other = (Restaurant) obj;
+		if (this.id == null) {
+			if (other.id != null) return false;
+		} else if (!this.id.equals(other.id)) return false;
+		return true;
+	}
 
-    public Address getAddress() {
-        return address;
-    }
+	public Address getAddress() {
+		return this.address;
+	}
 
-    public String getCompanyId() {
-        return companyId;
-    }
+	public String getCompanyId() {
+		return this.companyId;
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public String getDescription() {
+		return this.description;
+	}
 
-    @Override
-    public Long getId() {
-        return id;
-    }
+	@Override
+	public Long getId() {
+		return this.id;
+	}
 
-    public String getMainOffer() {
-        return mainOffer;
-    }
+	public String getMainOffer() {
+		return this.mainOffer;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return this.name;
+	}
 
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
+	public String getPhoneNumber() {
+		return this.phoneNumber;
+	}
 
-    public Set<FoodSpecialty> getSpecialties() {
-        return specialties;
-    }
+	public Set<FoodSpecialty> getSpecialties() {
+		return this.specialties;
+	}
 
-    public User getUser() {
-        return user;
-    }
+	public User getUser() {
+		return this.user;
+	}
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + (id == null ? 0 : id.hashCode());
-        return result;
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (this.id == null ? 0 : this.id.hashCode());
+		return result;
+	}
 
-    public boolean isHalal() {
-        return halal;
-    }
+	public boolean isHalal() {
+		return this.halal;
+	}
 
-    public boolean isKosher() {
-        return kosher;
-    }
+	public boolean isKosher() {
+		return this.kosher;
+	}
 
-    public boolean isVegetarian() {
-        return vegetarian;
-    }
+	public boolean isVegetarian() {
+		return this.vegetarian;
+	}
 
-    public void setAddress(final Address address) {
-        this.address = address;
-    }
+	public void setAddress(final Address address) {
+		this.address = address;
+	}
 
-    public void setCompanyId(final String companyId) {
-        this.companyId = companyId;
-    }
+	public void setCompanyId(final String companyId) {
+		this.companyId = companyId;
+	}
 
-    public void setDescription(final String description) {
-        this.description = description;
-    }
+	public void setDescription(final String description) {
+		this.description = description;
+	}
 
-    public void setHalal(final boolean halal) {
-        this.halal = halal;
-    }
+	public void setHalal(final boolean halal) {
+		this.halal = halal;
+	}
 
-    public void setId(final Long id) {
-        this.id = id;
-    }
+	public void setId(final Long id) {
+		this.id = id;
+	}
 
-    public void setKosher(final boolean kosher) {
-        this.kosher = kosher;
-    }
+	public void setKosher(final boolean kosher) {
+		this.kosher = kosher;
+	}
 
-    public void setMainOffer(final String mainOffer) {
-        this.mainOffer = mainOffer;
-    }
+	public void setMainOffer(final String mainOffer) {
+		this.mainOffer = mainOffer;
+	}
 
-    public void setName(final String name) {
-        this.name = name;
-    }
+	public void setName(final String name) {
+		this.name = name;
+	}
 
-    public void setPhoneNumber(final String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
+	public void setPhoneNumber(final String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
 
-    public void setSpecialties(final Set<FoodSpecialty> specialties) {
-        this.specialties = specialties;
-    }
+	public void setSpecialties(final Set<FoodSpecialty> specialties) {
+		this.specialties = specialties;
+	}
 
-    public void setUser(final User user) {
-        this.user = user;
-    }
+	public void setUser(final User user) {
+		this.user = user;
+	}
 
-    public void setVegetarian(final boolean vegetarian) {
-        this.vegetarian = vegetarian;
-    }
+	public void setVegetarian(final boolean vegetarian) {
+		this.vegetarian = vegetarian;
+	}
 
 }
--- midipascher-webapp/src/main/java/fr/midipascher/domain/User.java
+++ midipascher-webapp/src/main/java/fr/midipascher/domain/User.java
@@ -38,219 +38,223 @@ import fr.midipascher.domain.validation.Update;
 @XmlRootElement
 public class User extends AbstractEntity {
 
-    public static final String TABLE_NAME = "users";
-    public static final String TABLE_NAME_USER_AUTHORITY = "users_authority";
+	public static final String	TABLE_NAME						= "users";
+	public static final String	TABLE_NAME_USER_AUTHORITY		= "users_authority";
 
-    public static final String COLUMN_NAME_ID = "user_id";
-    public static final String COLUMN_NAME_LAST_CONNECTION = "last_connection";
-    public static final String COLUMN_NAME_FIRST_NAME = "first_name";
-    public static final String COLUMN_NAME_LAST_NAME = "last_name";
-    public static final String COLUMN_NAME_EMAIL = "email";
+	public static final String	COLUMN_NAME_ID					= "user_id";
+	public static final String	COLUMN_NAME_LAST_CONNECTION		= "last_connection";
+	public static final String	COLUMN_NAME_FIRST_NAME			= "first_name";
+	public static final String	COLUMN_NAME_LAST_NAME			= "last_name";
+	public static final String	COLUMN_NAME_EMAIL				= "email";
 
-    public static final int CONSTRAINT_FIRST_NAME_MAX_SIZE = 50;
-    private static final int CONSTRAINT_LAST_NAME_MAX_SIZE = 50;
-    public static final int CONSTRAINT_EMAIL_MAX_SIZE = 100;
-    public static final int CONSTRAINT_PASSWORD_MAX_SIZE = 200;
+	public static final int		CONSTRAINT_FIRST_NAME_MAX_SIZE	= 50;
+	private static final int	CONSTRAINT_LAST_NAME_MAX_SIZE	= 50;
+	public static final int		CONSTRAINT_EMAIL_MAX_SIZE		= 100;
+	public static final int		CONSTRAINT_PASSWORD_MAX_SIZE	= 200;
 
-    /**
+	/**
 	 * 
 	 */
-    private static final long serialVersionUID = -5952533696555432772L;
+	private static final long	serialVersionUID				= -5952533696555432772L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = User.COLUMN_NAME_ID)
-    @NotNull(message = "{user.id.required}", groups = { Update.class, Delete.class })
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = User.COLUMN_NAME_ID)
+	@NotNull(message = "{user.id.required}", groups = { Update.class, Delete.class })
+	private Long				id;
 
-    @Column(name = User.COLUMN_NAME_FIRST_NAME)
-    @NotEmpty(message = "{user.firstName.required}", groups = { Create.class, Update.class })
-    @Size(max = User.CONSTRAINT_FIRST_NAME_MAX_SIZE, message = "{user.firstName.max.size}", groups = { Create.class,
-            Update.class })
-    private String firstName;
+	@Column(name = User.COLUMN_NAME_FIRST_NAME)
+	@NotEmpty(message = "{user.firstName.required}", groups = { Create.class, Update.class })
+	@Size(max = User.CONSTRAINT_FIRST_NAME_MAX_SIZE, message = "{user.firstName.max.size}", groups = { Create.class,
+			Update.class })
+	private String				firstName;
 
-    @Column(name = User.COLUMN_NAME_LAST_NAME)
-    @NotEmpty(message = "{user.lastName.required}", groups = { Create.class, Update.class })
-    @Size(max = User.CONSTRAINT_LAST_NAME_MAX_SIZE, message = "{user.lastName.max.size}", groups = { Create.class,
-            Update.class })
-    private String lastName;
+	@Column(name = User.COLUMN_NAME_LAST_NAME)
+	@NotEmpty(message = "{user.lastName.required}", groups = { Create.class, Update.class })
+	@Size(max = User.CONSTRAINT_LAST_NAME_MAX_SIZE, message = "{user.lastName.max.size}", groups = { Create.class,
+			Update.class })
+	private String				lastName;
 
-    @NotNull(message = "{user.email.required}", groups = { Create.class, Update.class })
-    @Email(message = "{user.email.valid.format.required}", groups = { Create.class, Update.class })
-    @Size(max = User.CONSTRAINT_EMAIL_MAX_SIZE, message = "{user.email.max.size}", groups = { Create.class,
-            Update.class })
-    private String email;
+	@NotNull(message = "{user.email.required}", groups = { Create.class, Update.class })
+	@Email(message = "{user.email.valid.format.required}", groups = { Create.class, Update.class })
+	@Size(max = User.CONSTRAINT_EMAIL_MAX_SIZE, message = "{user.email.max.size}", groups = { Create.class,
+			Update.class })
+	private String				email;
 
-    @NotEmpty(message = "{user.password.required}", groups = { Create.class, Update.class })
-    @Size(max = User.CONSTRAINT_PASSWORD_MAX_SIZE, message = "{user.password.max.size}", groups = { Create.class,
-            Update.class })
-    private String password;
+	@NotEmpty(message = "{user.password.required}", groups = { Create.class, Update.class })
+	@Size(max = User.CONSTRAINT_PASSWORD_MAX_SIZE, message = "{user.password.max.size}", groups = { Create.class,
+			Update.class })
+	private String				password;
 
-    private boolean locked;
+	private boolean				locked;
 
-    @Type(type = "org.joda.time.contrib.hibernate.PersistentDateTime")
-    private DateTime created;
+	@Type(type = "org.joda.time.contrib.hibernate.PersistentDateTime")
+	private DateTime			created;
 
-    @Column(name = User.COLUMN_NAME_LAST_CONNECTION)
-    @Type(type = "org.joda.time.contrib.hibernate.PersistentDateTime")
-    private DateTime lastConnection;
+	@Column(name = User.COLUMN_NAME_LAST_CONNECTION)
+	@Type(type = "org.joda.time.contrib.hibernate.PersistentDateTime")
+	private DateTime			lastConnection;
 
-    @ManyToMany(cascade = CascadeType.ALL)
-    @JoinTable(name = User.TABLE_NAME_USER_AUTHORITY, joinColumns = { @JoinColumn(name = User.COLUMN_NAME_ID) }, inverseJoinColumns = { @JoinColumn(name = Authority.COLUMN_NAME_ID) })
-    @Valid
-    @NotEmpty(message = "{user.authorities.required}", groups = { Create.class, Update.class })
-    private Set<Authority> authorities;
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinTable(name = User.TABLE_NAME_USER_AUTHORITY, joinColumns = { @JoinColumn(name = User.COLUMN_NAME_ID) }, inverseJoinColumns = { @JoinColumn(name = Authority.COLUMN_NAME_ID) })
+	@Valid
+	@NotEmpty(message = "{user.authorities.required}", groups = { Create.class, Update.class })
+	private Set<Authority>		authorities;
 
-    @Override
-    public boolean equals(final Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        final User other = (User) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
-    }
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		final User other = (User) obj;
+		if (this.id == null) {
+			if (other.id != null) return false;
+		} else if (!this.id.equals(other.id)) return false;
+		return true;
+	}
 
-    /**
-     * @return the authorities
-     */
-    public Set<Authority> getAuthorities() {
-        return authorities;
-    }
+	/**
+	 * @return the authorities
+	 */
+	public Set<Authority> getAuthorities() {
+		return this.authorities;
+	}
 
-    /**
-     * @return the created
-     */
-    public DateTime getCreated() {
-        return created;
-    }
+	/**
+	 * @return the created
+	 */
+	public DateTime getCreated() {
+		return this.created;
+	}
 
-    /**
-     * @return the email
-     */
-    public String getEmail() {
-        return email;
-    }
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return this.email;
+	}
 
-    /**
-     * @return the firstName
-     */
-    public String getFirstName() {
-        return firstName;
-    }
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return this.firstName;
+	}
 
-    /**
-     * @return the id
-     */
-    @Override
-    public Long getId() {
-        return id;
-    }
+	/**
+	 * @return the id
+	 */
+	@Override
+	public Long getId() {
+		return this.id;
+	}
 
-    /**
-     * @return the lastConnection
-     */
-    public DateTime getLastConnection() {
-        return lastConnection;
-    }
+	/**
+	 * @return the lastConnection
+	 */
+	public DateTime getLastConnection() {
+		return this.lastConnection;
+	}
 
-    /**
-     * @return the lastName
-     */
-    public String getLastName() {
-        return lastName;
-    }
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return this.lastName;
+	}
 
-    /**
-     * @return the password
-     */
-    public String getPassword() {
-        return password;
-    }
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return this.password;
+	}
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + (id == null ? 0 : id.hashCode());
-        return result;
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (this.id == null ? 0 : this.id.hashCode());
+		return result;
+	}
 
-    /**
-     * @return the locked
-     */
-    public boolean isLocked() {
-        return locked;
-    }
+	/**
+	 * @return the locked
+	 */
+	public boolean isLocked() {
+		return this.locked;
+	}
 
-    /**
-     * @param authorities the authorities to set
-     */
-    public void setAuthorities(final Set<Authority> authorities) {
-        this.authorities = authorities;
-    }
+	/**
+	 * @param authorities
+	 *            the authorities to set
+	 */
+	public void setAuthorities(final Set<Authority> authorities) {
+		this.authorities = authorities;
+	}
 
-    /**
-     * @param created the created to set
-     */
-    public void setCreated(final DateTime created) {
-        this.created = created;
-    }
+	/**
+	 * @param created
+	 *            the created to set
+	 */
+	public void setCreated(final DateTime created) {
+		this.created = created;
+	}
 
-    /**
-     * @param email the email to set
-     */
-    public void setEmail(final String email) {
-        this.email = email;
-    }
+	/**
+	 * @param email
+	 *            the email to set
+	 */
+	public void setEmail(final String email) {
+		this.email = email;
+	}
 
-    /**
-     * @param firstName the firstName to set
-     */
-    public void setFirstName(final String firstName) {
-        this.firstName = firstName;
-    }
+	/**
+	 * @param firstName
+	 *            the firstName to set
+	 */
+	public void setFirstName(final String firstName) {
+		this.firstName = firstName;
+	}
 
-    /**
-     * @param id the id to set
-     */
-    public void setId(final Long id) {
-        this.id = id;
-    }
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(final Long id) {
+		this.id = id;
+	}
 
-    /**
-     * @param lastConnection the lastConnection to set
-     */
-    public void setLastConnection(final DateTime lastConnection) {
-        this.lastConnection = lastConnection;
-    }
+	/**
+	 * @param lastConnection
+	 *            the lastConnection to set
+	 */
+	public void setLastConnection(final DateTime lastConnection) {
+		this.lastConnection = lastConnection;
+	}
 
-    /**
-     * @param lastName the lastName to set
-     */
-    public void setLastName(final String lastName) {
-        this.lastName = lastName;
-    }
+	/**
+	 * @param lastName
+	 *            the lastName to set
+	 */
+	public void setLastName(final String lastName) {
+		this.lastName = lastName;
+	}
 
-    /**
-     * @param locked the locked to set
-     */
-    public void setLocked(final boolean locked) {
-        this.locked = locked;
-    }
+	/**
+	 * @param locked
+	 *            the locked to set
+	 */
+	public void setLocked(final boolean locked) {
+		this.locked = locked;
+	}
 
-    /**
-     * @param password the password to set
-     */
-    public void setPassword(final String password) {
-        this.password = password;
-    }
+	/**
+	 * @param password
+	 *            the password to set
+	 */
+	public void setPassword(final String password) {
+		this.password = password;
+	}
 
 }
