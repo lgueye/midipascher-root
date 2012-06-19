d6c9b9efd6b72e29c2f535a6ef0ae93b49389b43
louis gueye <louis.gueye@gmail.com>
2012-06-19 08:39:35 +0200

    @wip

---
+++ midipascher-webapp/src/test/java/fr/midipascher/stories/steps/PagesStatusSteps.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package fr.midipascher.stories.steps;
+
+import org.jbehave.core.annotations.Named;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+import org.junit.Assert;
+
+import com.sun.jersey.api.client.ClientResponse;
+
+/**
+ * @author louis.gueye@gmail.com
+ */
+public class PagesStatusSteps {
+
+	private ClientResponse	response;
+
+	@When("I send a \"<http_method>\" request at uri \"<uri>\"")
+	public void sendRequest(@Named("http_method") final String httpMethod, @Named("uri") final String uri) {
+		this.response = MidipascherClient.readURI(uri, "text/html", "fr");
+	}
+
+	@Then("the response code should be \"$statusCode\"")
+	public void expectStatusCode(@Named("statusCode") final int statusCode) {
+		Assert.assertEquals(statusCode, this.response.getStatus());
+	}
+}
