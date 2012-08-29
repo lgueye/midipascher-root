40f3db164a9977bb5fe8202eef19301fc1bba91d
louis gueye <louis.gueye@gmail.com>
2012-08-29 10:11:57 +0200

    @wip, refs #7: pass backend acceptance tests

--- midipascher-webapp/src/test/java/fr/midipascher/steps/backend/UpdateFoodSpecialtySteps.java
+++ midipascher-webapp/src/test/java/fr/midipascher/steps/backend/UpdateFoodSpecialtySteps.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package fr.midipascher.steps.backend;
 
@@ -24,79 +24,58 @@ import fr.midipascher.web.resources.FoodSpecialtiesResource;
 /**
  * @author louis.gueye@gmail.com
  */
-public class UpdateFoodSpecialtySteps {
-
-    private ClientResponse response;
-    private String responseLanguage;
-    private String responseContentType;
+public class UpdateFoodSpecialtySteps extends BackendBaseSteps {
 
     private static final String UPDATE_URI = UriBuilder.fromPath(WebConstants.BACKEND_PATH)
             .path(FoodSpecialtiesResource.COLLECTION_RESOURCE_PATH).path("5").build().toString();
 
-    @AfterScenario
-    public void afterScenario() {
-        responseLanguage = null;
-        responseContentType = null;
-    }
-
-    @Given("I authenticate with \"$uid\" uid and \"$password\" password")
-    public void authenticateWithWrongUid(@Named("uid") final String uid, @Named("password") final String password) {
-        MidipascherClient.setCredentials(uid, password);
-    }
+  public UpdateFoodSpecialtySteps(Exchange exchange) {
+    super(exchange);
+  }
 
     @Then("the message should be \"<message>\"")
     public void expectedMessage(@Named("message") final String message) {
-        Assert.assertEquals(message, response.getEntity(ResponseError.class).getMessage());
-    }
-
-    @Then("the response code should be \"$statusCode\"")
-    public void expectStatusCode(@Named("statusCode") final int statusCode) {
-        Assert.assertEquals(statusCode, response.getStatus());
+      this.exchange.assertExpectedMessage(ResponseError.class, message);
     }
 
     @When("I send a \"update food specialty\" request with wrong code \"<wrong_code>\"")
     public void sendUpdateFoodSpecialtyRequestWithWrongCode(@Named("wrong_code") final String code) {
         final FoodSpecialty foodSpecialty = TestFixtures.validFoodSpecialty();
         foodSpecialty.setCode(code);
-        final String requestContentType = "application/json";
-        response = MidipascherClient.updateEntity(foodSpecialty, UPDATE_URI, requestContentType, responseContentType,
-            responseLanguage);
+      Request request = this.exchange.getRequest();
+      request.setBody(foodSpecialty);
+      request.setUri(UPDATE_URI);
+      this.exchange.updateEntity();
     }
 
     @When("I send a \"update food specialty\" request with wrong id \"<wrong_id>\"")
     public void sendUpdateFoodSpecialtyRequestWithWrongId(@Named("wrong_id") final Long id) {
         final FoodSpecialty foodSpecialty = TestFixtures.validFoodSpecialty();
-        final String requestContentType = "application/json";
-        final String relativePath = WebConstants.BACKEND_PATH + FoodSpecialtiesResource.COLLECTION_RESOURCE_PATH + "/"
-            + id;
-        response = MidipascherClient.updateEntity(foodSpecialty, relativePath, requestContentType, responseContentType,
-            responseLanguage);
+      Request request = this.exchange.getRequest();
+      request.setBody(foodSpecialty);
+      final String relativePath = WebConstants.BACKEND_PATH + FoodSpecialtiesResource.COLLECTION_RESOURCE_PATH + "/"
+          + id;
+      request.setUri(relativePath);
+      this.exchange.updateEntity();
     }
 
     @When("I send a \"update food specialty\" request with wrong label \"<wrong_label>\"")
     public void sendUpdateFoodSpecialtyRequestWithWrongLabel(@Named("wrong_label") final String label) {
         final FoodSpecialty foodSpecialty = TestFixtures.validFoodSpecialty();
         foodSpecialty.setLabel(label);
-        final String requestContentType = "application/json";
-        response = MidipascherClient.updateEntity(foodSpecialty, UPDATE_URI, requestContentType, responseContentType,
-            responseLanguage);
+      Request request = this.exchange.getRequest();
+      request.setBody(foodSpecialty);
+      request.setUri(UPDATE_URI);
+      this.exchange.updateEntity();
     }
 
     @When("I send a valid \"update food specialty\" request")
     public void sendValidUpdateFoodSpecialtyRequest() {
         final FoodSpecialty foodSpecialty = TestFixtures.validFoodSpecialty();
-        final String requestContentType = "application/json";
-        response = MidipascherClient.updateEntity(foodSpecialty, UPDATE_URI, requestContentType, responseContentType,
-            responseLanguage);
+      Request request = this.exchange.getRequest();
+      request.setBody(foodSpecialty);
+      request.setUri(UPDATE_URI);
+      this.exchange.updateEntity();
     }
 
-    @Given("I accept \"$responseContentType\" format")
-    public void setFormat(@Named("responseContentType") final String responseContentType) {
-        this.responseContentType = responseContentType;
-    }
-
-    @Given("I accept \"$responseLanguage\" language")
-    public void setLanguage(@Named("responseLanguage") final String responseLanguage) {
-        this.responseLanguage = responseLanguage;
-    }
 }
--- midipascher-webapp/src/test/resources/fr/midipascher/stories/backend/update/update_food_specialty.story
+++ midipascher-webapp/src/test/resources/fr/midipascher/stories/backend/update/update_food_specialty.story
@@ -2,7 +2,7 @@ Update food specialties stories
 
 Meta:
 @refs 7
-@progress done
+@progress wip
 
 Narrative:
 Given I provide a update food specialty request
@@ -10,53 +10,61 @@ When I send that request
 It should either succeed or fail
 
 Scenario: update food specialty should succeed
-Given I provide "admin@admin.com" uid and "secret" password
+Given I provide "<uid>" uid and "<password>" password
 When I send a valid "update food specialty" request
 Then the response code should be "200"
 
+Examples:
+|uid|password|
+|admin@admin.com|secret|
+
 Scenario: update food specialty as account with wrong role should fail
-Given I provide "rmgr@rmgr.com" uid and "secret" password
-And I accept "fr" language
-And I accept "application/json" format
+Given I provide "<uid>" uid and "<password>" password
+And I accept "<responseLanguage>" language
+And I accept "<responseContentType>" format
 When I send a valid "update food specialty" request
 Then the response code should be "403"
 And the message should be "<message>"
+
 Examples:
-|message|
-|Accès refusé|
+|uid|password|responseContentType|responseLanguage|message|
+|rmgr@rmgr.com|secret|application/json|fr|Accès refusé|
 
 Scenario: update food specialty with wrong id should fail
-Given I provide "admin@admin.com" uid and "secret" password
-And I accept "en" language
-And I accept "application/xml" format
+Given I provide "<uid>" uid and "<password>" password
+And I accept "<responseLanguage>" language
+And I accept "<responseContentType>" format
 When I send a "update food specialty" request with wrong id "<wrong_id>"
 Then the response code should be "404"
 And the message should be "<message>"
+
 Examples:
-|wrong_id|message|
-|-1|Food specialty [id = -1] was not found|
+|uid|password|responseContentType|responseLanguage|wrong_id|message|
+|admin@admin.com|secret|application/json|en|-1|Food specialty [id = -1] was not found|
 
 Scenario: update food specialty with wrong code should fail
-Given I provide "admin@admin.com" uid and "secret" password
-And I accept "en" language
-And I accept "application/json" format
+Given I provide "<uid>" uid and "<password>" password
+And I accept "<responseLanguage>" language
+And I accept "<responseContentType>" format
 When I send a "update food specialty" request with wrong code "<wrong_code>"
 Then the response code should be "400"
 And the message should be "<message>"
+
 Examples:
-|wrong_code|message|
-||Code is required|
-|dlfjyyprpmz|Code max length is 10|
-|SDW|Code "SDW" already used|
+|uid|password|responseContentType|responseLanguage|wrong_code|message|
+|admin@admin.com|secret|application/json|en||Code is required|
+|admin@admin.com|secret|application/json|en|dlfjyyprpmz|Code max length is 10|
+|admin@admin.com|secret|application/json|en|SDW|Code "SDW" already used|
 
 Scenario: update food specialty with wrong label should fail
-Given I provide "admin@admin.com" uid and "secret" password
-And I accept "en" language
-And I accept "application/json" format
+Given I provide "<uid>" uid and "<password>" password
+And I accept "<responseLanguage>" language
+And I accept "<responseContentType>" format
 When I send a "update food specialty" request with wrong label "<wrong_label>"
 Then the response code should be "400"
 And the message should be "<message>"
+
 Examples:
-|wrong_label|message|
-||Label is required|
-|aaaaaaaaaaBBBBBBBBBBuuuuuuuuuu55555555559999999999S|Label max length is 50|
+|uid|password|responseContentType|responseLanguage|wrong_label|message|
+|admin@admin.com|secret|application/json|en||Label is required|
+|admin@admin.com|secret|application/json|en|aaaaaaaaaaBBBBBBBBBBuuuuuuuuuu55555555559999999999S|Label max length is 50|
