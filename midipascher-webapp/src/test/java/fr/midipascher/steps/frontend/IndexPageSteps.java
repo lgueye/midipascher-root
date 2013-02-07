42bcf9ab49dcf2ce9a04d3672b7ab0bc130af0d5
louis gueye <louis.gueye@gmail.com>
2013-02-07 18:27:28 +0100

    add steps

--- midipascher-webapp/src/test/java/fr/midipascher/steps/frontend/IndexPageSteps.java
+++ midipascher-webapp/src/test/java/fr/midipascher/steps/frontend/IndexPageSteps.java
@@ -1,8 +1,44 @@
 package fr.midipascher.steps.frontend;
 
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Named;
+import org.jbehave.core.annotations.Pending;
+import org.jbehave.core.annotations.Then;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
 /**
- * User: lgueye Date: 07/02/13 Time: 17:50
+ * @author louis.gueye@gmail.com
  */
 public class IndexPageSteps {
+
+  private HtmlUnitDriver htmlUnitDriver;
+
+  public IndexPageSteps(HtmlUnitDriver htmlUnitDriver) {
+    this.htmlUnitDriver = htmlUnitDriver;
+  }
+
+  @Given("I navigate to the \"$pageId\" page")
+  public void navigateTo(@Named("pageId") String pageId) {
+    assertTrue(false);
+  }
+
+  @Then("I should be able to click the \"$linkId\" link")
+  public void findLinkById(@Named("linkId") String linkId) {
+    assertTrue(false);
+  }
+
+  @Then("I should be able to click the \"$buttonId\" button")
+  public void findButtonById(@Named("buttonId") String buttonId) {
+    assertTrue(false);
+  }
+
+  @Then("I should be able to input the \"$inputId\"")
+  public void findInputById(@Named("inputId") String inputId) {
+    assertTrue(false);
+  }
 
 }
