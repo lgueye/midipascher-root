4ef5029d08d850ac3bf3bb48bce39f3616619347
louis gueye <louis.gueye@gmail.com>
2013-02-07 18:26:48 +0100

    Add HtmlUnitWebDriver

---
+++ midipascher-webapp/src/test/java/fr/midipascher/steps/frontend/IndexPageSteps.java
@@ -0,0 +1,8 @@
+package fr.midipascher.steps.frontend;
+
+/**
+ * User: lgueye Date: 07/02/13 Time: 17:50
+ */
+public class IndexPageSteps {
+
+}
---
+++ midipascher-webapp/src/test/java/fr/midipascher/stories/frontend/IndexPageStory.java
@@ -0,0 +1,8 @@
+package fr.midipascher.stories.frontend;
+
+/**
+ * User: lgueye Date: 07/02/13 Time: 17:46
+ */
+public class IndexPageStory {
+
+}
